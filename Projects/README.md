0c0ce70da91c8a1d10559b317912f6c5e19b381f
Sarah <saraalsuabie11.a@gmail.com>
2026-08-12 02:05:24 +0300

    Update

---
+++ Projects/README.md
@@ -0,0 +1,2 @@
+![alt text](<Screenshot 2026-08-02 191400.png>)
+![alt text](<Screenshot 2026-08-02 192608.png>)
---
+++ Projects/setup.ps1
@@ -0,0 +1,9 @@
+write-host "Gernarate The Project Structure .. "
+mkdir src 
+
+ni src/main.py
+ni README.md 
+
+git init 
+
+write-host " is done!"
